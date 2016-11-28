7bfb297433f7f82f4e6b137f9c3220d1ab3df620
Andrew Rangel <antifragiledevelopment@gmail.com>
2016-11-27 22:11:07 -0600

    Added documentation using ex_docs

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,12 @@
 defmodule Cards do
 
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -10,23 +17,47 @@ defmodule Cards do
 
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck
+    The `hand_size` arguement indicates how many cards should be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Returns a deck that has been shuffled
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Returns a bool that will be true if card exists in the deck
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Saves a deck to the designated filename
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loads a deck from the designated filename
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -34,6 +65,10 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Pipes together create_deck \> shuffle \> deal
+    Returns a hand of the designated size 
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
